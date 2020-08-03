13769c7ed4c4f39b9dc1a3370e99c978d87a37bc
Ian Duncan <janskykd@gmail.com>
2020-08-03 19:17:48 +0800

    automated deployment

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ if ! file lambda_package/cartogram | grep -q "GNU/Linux"; then
 fi
 
 ./package.sh
-aws lambda update-function-code --function-name $LAMBDA_FUNCTION_NAME --zip-file fileb://cartogram.zip --only-show-errors || exit 1
+aws lambda update-function-code --function-name $LAMBDA_FUNCTION_NAME --zip-file fileb://cartogram.zip > /dev/null 2>&1 || exit 1
 rm cartogram.zip
 
 set +x
